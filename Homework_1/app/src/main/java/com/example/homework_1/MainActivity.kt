6aa6889266f464b325c4a0d73dc269bf11268d63
ElindTahir <95304227+ElindTahir@users.noreply.github.com>
2023-03-22 23:20:28 +0100

    Task 4

--- Homework_1/app/src/main/java/com/example/homework_1/MainActivity.kt
+++ Homework_1/app/src/main/java/com/example/homework_1/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.homework_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import com.google.android.material.textfield.TextInputEditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firstNumber: EditText
+    private lateinit var secondNumber: EditText
+    private lateinit var result: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    firstNumber = findViewById(R.id.firstNumber)
+    secondNumber = findViewById(R.id.secondNumber)
+    result = findViewById(R.id.resultView)
 
+    val calcButton = findViewById<Button>(R.id.calcButton)
+
+    calcButton.setOnClickListener {
+        val numberOne = firstNumber.text.toString().toIntOrNull() ?: 0
+        val numberTwo = secondNumber.text.toString().toIntOrNull() ?: 0
+
+        val sum = numberOne + numberTwo
+        result.text = sum.toString()
     }
 
-    var firstNumber = findViewById<TextInputEditText>(R.id.firstNumber)
-    var secondNumber = findViewById<TextInputEditText>(R.id.secondNumber)
-    var resultView = findViewById<TextView>(R.id.resultView)
 
+
+
+
+
+
+
+    }
 }
