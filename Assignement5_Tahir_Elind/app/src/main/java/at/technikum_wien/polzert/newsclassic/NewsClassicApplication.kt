150bdbce471485ca4bbbe79d41e769ae2706fc82
ElindTahir <if21b033@technikum-wien.at>
2024-05-15 22:22:40 +0200

    Homework5

--- Assignement5_Tahir_Elind/app/src/main/java/at/technikum_wien/polzert/newsclassic/NewsClassicApplication.kt
+++
@@ -1,8 +0,0 @@
-package at.technikum_wien.polzert.newsclassic
-
-import android.app.Application
-import at.technikum_wien.polzert.newsclassic.data.db.ItemDatabase
-
-class NewsClassicApplication : Application() {
-    val database: ItemDatabase by lazy { ItemDatabase.getDatabase(this) }
-}
